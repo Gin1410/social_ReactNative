e9eea3d990d973bfd700aaf01a0239a92126ced0
ptnquynh <ptnquynhta@gmail.com>
2023-11-22 00:03:36 +0700

    dropdown post

--- components/Home/PostHeader.js
+++ components/Home/PostHeader.js
@@ -13,6 +13,7 @@ const PostHeader = ({ post }) => {
 
     const [visible, setVisible] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
+    console.log(post);
 
     const openMenu = () => {
         setVisible(true);
@@ -52,42 +53,42 @@ const PostHeader = ({ post }) => {
     }
 
     return (
-        <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", margin: 5}}>
+        <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", margin: 5 }}>
 
             <View style={{ flexDirection: "row", alignItem: "center", marginTop: 7, marginLeft: 7 }}>
-                <Image source={{ uri: post.avatar  }} style={{ width: 35, height: 35, borderRadius: 50, borderWidth: 1, borderColor: "white" }} />
+                <Image source={{ uri: post.avatar }} style={{ width: 35, height: 35, borderRadius: 50, borderWidth: 1, borderColor: "white" }} />
                 <Text style={{ color: "white", marginLeft: 10, fontWeight: '700', textAlignVertical: "center", fontSize: 17 }}>{post.name}</Text>
             </View>
 
-            <View
-                style={{
-                    paddingTop: 10,
-                }}
-            >
-                <Menu
+            {post.follow_id == null && (
+                <View
                     style={{
-                        top: 30,
-                        left: 325,
-                        marginLeft: -100,
+                        paddingTop: 10,
                     }}
-                    visible={visible}
-                    onDismiss={closeMenu}
-                    anchor={
-                        <Button onPress={openMenu}>
-                            <MaterialCommunityIcons
-                                name={visible ? 'menu-up' : 'menu-down'}
-                                size={35}
-                                color="white"
-                            />
-                        </Button>
-                    }
                 >
-                    <Menu.Item onPress={handleDelete} title="Update" />
-                    <Menu.Item onPress={handleDelete} title="Delete" />
-                </Menu>
-
-            </View>
-
+                    <Menu
+                        style={{
+                            top: 30,
+                            left: 325,
+                            marginLeft: -100,
+                        }}
+                        visible={visible}
+                        onDismiss={closeMenu}
+                        anchor={
+                            <Button onPress={openMenu}>
+                                <MaterialCommunityIcons
+                                    name={visible ? 'menu-up' : 'menu-down'}
+                                    size={35}
+                                    color="white"
+                                />
+                            </Button>
+                        }
+                    >
+                        <Menu.Item onPress={handleDelete} title="Update" />
+                        <Menu.Item onPress={handleDelete} title="Delete" />
+                    </Menu>
+                </View>
+            )}
         </View>
     )
 }
